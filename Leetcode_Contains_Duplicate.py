6a51f086b446764dc61bd93d692ee342e2c288cd
Md. Mahib Hosen Ornob <32245036+MahibArnob@users.noreply.github.com>
2022-12-31 14:56:11 +0600

    Create Leetcode_Contains_Duplicate.py

---
+++ Leetcode_Contains_Duplicate.py
@@ -0,0 +1,8 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashmap = set()
+        for i in nums:
+            if i in hashmap:
+                return True
+            hashmap.add(i)
+        return False
