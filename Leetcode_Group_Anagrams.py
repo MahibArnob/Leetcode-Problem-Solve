fbc948f846f143ea16bfae22ed45dede72e11fb1
Md. Mahib Hosen Ornob <32245036+MahibArnob@users.noreply.github.com>
2023-01-02 21:02:56 +0600

    Update Leetcode_Group_Anagrams.py

--- Leetcode_Group_Anagrams.py
+++ Leetcode_Group_Anagrams.py
@@ -11,3 +11,25 @@ class Solution:
             result[tuple(count)].append(s)
         
         return result.values()
+    
+    
+### Second Way ###
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dict = {}
+        
+        for word in strs:
+            sortedWord = "".join(sorted(word))
+            
+            if sortedWord not in dict:
+                dict[sortedWord] = [word]
+            
+            else:
+                dict[sortedWord].append(word)
+            
+        result = []
+        for item in dict.values():
+            result.append(item)
+        
+        return result
