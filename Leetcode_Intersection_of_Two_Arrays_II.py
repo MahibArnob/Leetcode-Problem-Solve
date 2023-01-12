1bfc4dcd0fe08df5edf79b9090e6e9b15773bfb1
Md. Mahib Hosen Ornob <32245036+MahibArnob@users.noreply.github.com>
2023-01-12 15:37:29 +0600

    Update Leetcode_Intersection_of_Two_Arrays_II.py

--- Leetcode_Intersection_of_Two_Arrays_II.py
+++ Leetcode_Intersection_of_Two_Arrays_II.py
@@ -1,3 +1,5 @@
+### Approach Two - Using hashmap ###
+
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
         if len(nums1) > len(nums2): return self.intersect(nums2, nums1)
@@ -9,3 +11,23 @@ class Solution:
                 ans.append(x)
                 cnt[x] -= 1
         return ans
+
+### Approach Two - Using sort & two pointers ###
+
+class Solution:
+def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+    nums1.sort()
+    nums2.sort()
+
+    ans = []
+    i = j = 0
+    while i < len(nums1) and j < len(nums2):
+        if nums1[i] < nums2[j]:
+            i += 1
+        elif nums1[i] > nums2[j]:
+            j += 1
+        else:
+            ans.append(nums1[i])
+            i += 1
+            j += 1
+    return ans
