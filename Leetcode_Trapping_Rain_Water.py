4470e39c3ab191589dd79ad9621b63541bdf24a4
Md. Mahib Hosen Ornob <32245036+MahibArnob@users.noreply.github.com>
2023-01-19 22:18:52 +0600

    Create Leetcode_Trapping_Rain_Water.py

---
+++ Leetcode_Trapping_Rain_Water.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        left, right = 0, len(height) - 1
+        result = 0
+        leftMax, rightMax = height[left], height[right]
+
+        while left < right:
+            if leftMax <= rightMax:
+                left += 1
+                leftMax = max(leftMax, height[left])
+                result += leftMax - height[left]
+            else:
+                right -= 1
+                rightMax = max(rightMax, height[right])
+                result += rightMax - height[right]
+                
+        return result
