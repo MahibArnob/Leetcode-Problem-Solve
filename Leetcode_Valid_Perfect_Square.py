750fff1745d8702affac622ccad25dbae869ab17
Md. Mahib Hosen Ornob <32245036+MahibArnob@users.noreply.github.com>
2023-02-08 15:58:02 +0600

    Update Leetcode_Valid_Perfect_Square.py

--- Leetcode_Valid_Perfect_Square.py
+++ Leetcode_Valid_Perfect_Square.py
@@ -9,3 +9,24 @@ class Solution:
             return True
         else:
             return False
+
+        
+        
+ # Another Approach Using Binary Tree
+
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        if num == 1:
+            return True
+        lo = 2
+        hi = num // 2
+        while lo <= hi:
+            mid = lo + (hi - lo) //2
+            print(mid)
+            if mid * mid == num:
+                return True
+            if mid * mid > num:
+                hi = mid - 1
+            else:
+                lo = mid + 1
+        return False
