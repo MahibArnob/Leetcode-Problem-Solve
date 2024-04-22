33e4b735f1f58d086be11704c31d6b8a157201a6
Md. Mahib Hosen Ornob <32245036+MahibArnob@users.noreply.github.com>
2024-04-22 23:06:40 +0600

    Update Leetcode_Product_of_Array_Except_Self.py

--- Leetcode_Product_of_Array_Except_Self.py
+++ Leetcode_Product_of_Array_Except_Self.py
@@ -1,14 +1,14 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        result = [1] * (len(nums))
+        result = [1] * (len(nums)) # [1, 1, 1, 1]
         # nums ... [1,2,3,4]
         prefix = 1
         postfix = 1
-        # inserting the prefix value to result and multiplying it with current number for next iteration. ...[1,1,2,6]
+        # Inserting the prefix value to the result and multiplying it with the current number for the next iteration. ...[1,1,2,6]
         for i in range(len(nums)):
             result[i] = prefix
             prefix *= nums[i]
-        # In reverse order multiplying the postfix value to result and storing it in place of result, for next iteration multiplying it with current number ..[24,12,8,6]
+        # In reverse order multiply the postfix value to the result and store it in place of result, for next iteration multiplying it with current number ..[24,12,8,6]
         for j in range(len(nums) -1, -1, -1):
             result[j] *= postfix
             postfix *= nums[j]
