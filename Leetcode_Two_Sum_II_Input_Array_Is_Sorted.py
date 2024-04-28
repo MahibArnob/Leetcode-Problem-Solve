bdc3870b9e5d613a49ccc23963bdfb08699f7a3f
Md. Mahib Hosen Ornob <32245036+MahibArnob@users.noreply.github.com>
2024-04-28 21:47:36 +0600

    Update Leetcode_Two_Sum_II_Input_Array_Is_Sorted.py

--- Leetcode_Two_Sum_II_Input_Array_Is_Sorted.py
+++ Leetcode_Two_Sum_II_Input_Array_Is_Sorted.py
@@ -11,3 +11,22 @@ class Solution:
                 left += 1
             else:
                 return [left + 1, right + 1]
+
+
+### 2nd Approach: Using Dict ###
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        seen = {}
+        
+        for i, num in enumerate(numbers):
+            complement = target - num
+            
+            if complement in seen:
+                # If complement exists, return their indices incremented by one
+                return [seen[complement] + 1, i + 1]
+            
+            # Add current index to the dictionary
+            seen[num] = i
+        
+        # If no solution is found, return an empty list
+        return []
